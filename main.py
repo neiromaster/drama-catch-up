a9767df17dc3ac3cc729aa024a89a351da4ba1ba
Maxim (neiromaster) Gavrilenko <parrysh@mail.ru>
2025-08-31 22:18:50 +0500

    chore: Rename project to drama-catch-up
    - Update the project name in pyproject.toml.
    - Update artifact and binary names in GitHub Actions workflows to reflect the new project name.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import time
 from src.app import run_check
 
+
 def main():
     """The main entry point of the script."""
     try:
         print("🚀 Мониторинг запущен. Нажмите Ctrl+C для выхода.")
         while True:
             interval_minutes = run_check()
-            print(f"\n---\n🕒 Проверка завершена. Следующая проверка через {interval_minutes} минут.")
+            print(
+                f"\n---\n🕒 Проверка завершена. Следующая проверка через {interval_minutes} минут."
+            )
             time.sleep(interval_minutes * 60)
     except KeyboardInterrupt:
         print("\n\n🛑 Получен сигнал завершения. Выход.")
+
 
 if __name__ == "__main__":
     main()
